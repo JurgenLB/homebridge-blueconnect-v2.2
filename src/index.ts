6539171a00b2dc7792448853e2b94ab677adf9e0
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-06-01 01:57:11 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
 import type { API } from 'homebridge';
 import { registerCustomCharacteristicsAndServices } from './customCharacteristics';
-
 import { BlueConnectPlatform } from './blueConnectPlatform.js';
 import { PLATFORM_NAME } from './settings.js';
 
 /**
  * This method registers the platform with Homebridge
  */
-export function registerCustomCharacteristicsAndServices (api: API) {
+export = (api: API) => {
+  registerCustomCharacteristicsAndServices(api);
   api.registerPlatform(PLATFORM_NAME, BlueConnectPlatform);
-  api.hap.Service.ConductivitySensor = ConductivitySensorService;
-  api.hap.Characteristic.Conductivity = ConductivityCharacteristic;
-  api.hap.Service.PhSensor = PhSensorService;
-  api.hap.Characteristic.Ph = PhCharacteristic;
-  api.hap.Service.OrpSensor = OrpSensorService;
-  api.hap.Characteristic.Orp = OrpCharacteristic;
 };
 
