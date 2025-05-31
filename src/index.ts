d7da66f71251e0dc60985fde832df3d01ea0426c
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-05-31 20:34:24 +0200

    register Custom Characteristics

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import type { API } from 'homebridge';
 
 import { BlueConnectPlatform } from './blueConnectPlatform.js';
 import { PLATFORM_NAME } from './settings.js';
+import { registerCustomCharacteristics } from './customCharacteristics';
 
 /**
  * This method registers the platform with Homebridge
  */
 export default (api: API) => {
+  registerCustomCharacteristics(api);
   api.registerPlatform(PLATFORM_NAME, BlueConnectPlatform);
 };
 
