e535054df7e9a9e5cf4686457b335d0878f4c831
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-06-01 01:31:35 +0200

    Update Characteristic

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,13 @@ import { PLATFORM_NAME } from './settings.js';
 /**
  * This method registers the platform with Homebridge
  */
-export default (api: API) => {
-  registerCustomCharacteristicsAndServices(api);
+export function registerCustomCharacteristicsAndServices (api: API) => {
   api.registerPlatform(PLATFORM_NAME, BlueConnectPlatform);
+  api.hap.Service.ConductivitySensor = ConductivitySensorService;
+  api.hap.Characteristic.Conductivity = ConductivityCharacteristic;
+  api.hap.Service.PhSensor = PhSensorService;
+  api.hap.Characteristic.Ph = PhCharacteristic;
+  api.hap.Service.OrpSensor = OrpSensorService;
+  api.hap.Characteristic.Orp = OrpCharacteristic;
 };
 
