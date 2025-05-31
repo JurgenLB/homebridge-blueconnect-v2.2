649677510f067906e827feaaacbbdf7c8c2634cc
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-06-01 01:12:35 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import type { API } from 'homebridge';
+import { registerCustomCharacteristicsAndServices } from './customCharacteristics';
 
 import { BlueConnectPlatform } from './blueConnectPlatform.js';
 import { PLATFORM_NAME } from './settings.js';
-import { registerCustomCharacteristicsAndServices } from './customCharacteristics';
 
 /**
  * This method registers the platform with Homebridge
