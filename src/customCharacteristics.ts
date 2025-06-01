8c7c9c92afe2fe760a0eede1ec0f384eb91c28b5
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-06-01 19:14:27 +0200

    Update customCharacteristics.ts

--- src/customCharacteristics.ts
+++ src/customCharacteristics.ts
@@ -12,94 +12,83 @@ interface BlueDevice {
   // "battery_low": false,
 };
 
+// Conductivity
+class ConductivityCharacteristic extends (require('hap-nodejs').Characteristic) {
+  static readonly UUID: string;
+  constructor(api: API, blueDevice: BlueDevice) {
+    super('Conductivity', api.hap.uuid.generate('conductivity-' + blueDevice.blue_device_serial), {
+      format: Formats.FLOAT,
+      unit: 'µS/cm',
+      minValue: 0,
+      maxValue: 2000,
+      minStep: 1,
+      perms: [
+        Perms.PAIRED_READ,
+        Perms.NOTIFY,
+      ],
+    });
+  }
+}
+
+// PH
+class PhCharacteristic extends (require('hap-nodejs').Characteristic) {
+  static readonly UUID: string;
+  constructor(api: API, blueDevice: BlueDevice) {
+    super('pH', api.hap.uuid.generate('ph-' + blueDevice.blue_device_serial), {
+      format: Formats.FLOAT,
+      unit: '',
+      minValue: 0,
+      maxValue: 20,
+      minStep: 0.01,
+      perms: [
+        Perms.PAIRED_READ,
+        Perms.NOTIFY,
+      ],
+    });
+  }
+}
+
+// ORP
+class OrpCharacteristic extends (require('hap-nodejs').Characteristic) {
+  static readonly UUID: string;
+  constructor(api: API, blueDevice: BlueDevice) {
+    super('ORP', api.hap.uuid.generate('orp-' + blueDevice.blue_device_serial), {
+      format: Formats.FLOAT,
+      unit: 'mV',
+      minValue: 0,
+      maxValue: 2000,
+      minStep: 1,
+      perms: [
+        Perms.PAIRED_READ,
+        Perms.NOTIFY,
+      ],
+    });
+  }
+}
+
+
 export function createCustomCharacteristicsAndServices(api: API, blueDevice: BlueDevice) {
   // Conductivity
-  const conductivityCharacteristicUUID = api.hap.uuid.generate('conductivity-' + blueDevice.blue_device_serial);
-  const conductivityServiceUUID = api.hap.uuid.generate('conductivity-service-' + blueDevice.blue_device_serial);
-
-  class ConductivityCharacteristic extends api.hap.Characteristic {
-    public static readonly UUID = conductivityCharacteristicUUID;
-
-    constructor() {
-      super('Conductivity', conductivityCharacteristicUUID, {
-        format: Formats.FLOAT,
-        unit: 'µS/cm',
-        minValue: 0,
-        maxValue: 2000,
-        minStep: 1,
-        perms: [
-          Perms.PAIRED_READ,
-          Perms.NOTIFY,
-        ],
-      });
-
-    }
-  }
-
   class ConductivitySensorService extends api.hap.Service {
     constructor(displayName: string, subtype?: string) {
-      super(displayName, conductivityServiceUUID, subtype);
-      this.addCharacteristic(ConductivityCharacteristic);
+      super(displayName, api.hap.uuid.generate('conductivity-service-' + blueDevice.blue_device_serial), subtype);
+      this.addCharacteristic(new ConductivityCharacteristic(api, blueDevice));
     }
   }
 
-  // pH
-  const phCharacteristicUUID = api.hap.uuid.generate('ph-' + blueDevice.blue_device_serial);
-  const phServiceUUID = api.hap.uuid.generate('ph-service-' + blueDevice.blue_device_serial);
-
-  class PhCharacteristic extends api.hap.Characteristic {
-    public static readonly UUID = phCharacteristicUUID;
-
-    constructor() {
-      super('pH', phCharacteristicUUID, {
-        format: Formats.FLOAT,
-        unit: '',
-        minValue: 0,
-        maxValue: 20,
-        minStep: 0.01,
-        perms: [
-          Perms.PAIRED_READ,
-          Perms.NOTIFY,
-        ],
-      });
-
-    }
-  }
-
+  // PH
   class PhSensorService extends api.hap.Service {
     constructor(displayName: string, subtype?: string) {
-      super(displayName, phServiceUUID, subtype);
-      this.addCharacteristic(PhCharacteristic);
+      super(displayName, api.hap.uuid.generate('ph-service-' + blueDevice.blue_device_serial), subtype);
+      this.addCharacteristic(new PhCharacteristic(api, blueDevice));
     }
   }
 
   // ORP
-  const orpCharacteristicUUID = api.hap.uuid.generate('orp-' + blueDevice.blue_device_serial);
-  const orpServiceUUID = api.hap.uuid.generate('orp-service-' + blueDevice.blue_device_serial);
-
-  class OrpCharacteristic extends api.hap.Characteristic {
-    public static readonly UUID = orpCharacteristicUUID;
-
-    constructor() {
-      super('ORP', orpCharacteristicUUID, {
-        format: Formats.FLOAT,
-        unit: 'mV',
-        minValue: 0,
-        maxValue: 2000,
-        minStep: 1,
-        perms: [
-          Perms.PAIRED_READ,
-          Perms.NOTIFY,
-        ],
-      });
-
-    }
-  }
-
   class OrpSensorService extends api.hap.Service {
     constructor(displayName: string, subtype?: string) {
-      super(displayName, orpServiceUUID, subtype);
-      this.addCharacteristic(OrpCharacteristic);
+      super(displayName, api.hap.uuid.generate('orp-service-' + blueDevice.blue_device_serial), subtype);
+      this.addCharacteristic(new OrpCharacteristic(api, blueDevice));
     }
   }
 
