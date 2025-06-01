c67804413f08efd687bfb8efe415c5b657335105
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-06-01 10:52:52 +0200

    Update customCharacteristics.ts

--- src/customCharacteristics.ts
+++ src/customCharacteristics.ts
@@ -1,99 +1,99 @@
 import { Service, Characteristic } from 'homebridge';
 
-const ConductivityServiceUUID = this.api.hap.uuid.generate('conductivity-service-' + blueDevice.blue_device_serial);
-const PhServiceUUID = this.api.hap.uuid.generate('ph-service-' + blueDevice.blue_device_serial);
-const OrpServiceUUID = this.api.hap.uuid.generate('orp-service-' + blueDevice.blue_device_serial);
+export function createCustomCharacteristicsAndServices(api, blueDevice) {
+  // Conductivity
+  const conductivityCharacteristicUUID = api.hap.uuid.generate('conductivity-' + blueDevice.blue_device_serial);
+  const conductivityServiceUUID = api.hap.uuid.generate('conductivity-service-' + blueDevice.blue_device_serial);
 
-
-// --- Custom Characteristics ---
-// Conductivity
-export function createConductivityCharacteristic(api, blueDevice) {
-  const uuid = api.hap.uuid.generate('conductivity-' + blueDevice.blue_device_serial);
-  return class ConductivityCharacteristic extends api.hap.Characteristic {
+  class ConductivityCharacteristic extends api.hap.Characteristic {
     constructor() {
-      super('Conductivity', uuid);
+      super('Conductivity', conductivityCharacteristicUUID);
       this.setProps({
-        format: Characteristic.Formats.FLOAT,
+        format: api.hap.Characteristic.Formats.FLOAT,
         unit: 'µS/cm',
         minValue: 0,
         maxValue: 2000,
         minStep: 1,
-        perms: [api.hap.Characteristic.Perms.PAIRED_READ, api.hap.Characteristic.Perms.NOTIFY],
+        perms: [
+          api.hap.Characteristic.Perms.PAIRED_READ,
+          api.hap.Characteristic.Perms.NOTIFY,
+        ],
       });
       this.value = this.getDefaultValue();
     }
-  };
-};
-
-// Define a custom Conductivity Sensor Service
-export class ConductivitySensorService extends this.api.hap.Service {
-  constructor(displayName: string, subtype?: string) {
-    super(displayName, ConductivityServiceUUID, subtype);
-    this.addCharacteristic(ConductivityCharacteristic);
   }
-}
 
+  class ConductivitySensorService extends api.hap.Service {
+    constructor(displayName: string, subtype?: string) {
+      super(displayName, conductivityServiceUUID, subtype);
+      this.addCharacteristic(ConductivityCharacteristic);
+    }
+  }
 
-// pH
-export function createPhCharacteristic(api, blueDevice) {
-  const uuid = api.hap.uuid.generate('ph-' + blueDevice.blue_device_serial);
-  return class PhCharacteristic extends api.hap.Characteristic {
+  // pH
+  const phCharacteristicUUID = api.hap.uuid.generate('ph-' + blueDevice.blue_device_serial);
+  const phServiceUUID = api.hap.uuid.generate('ph-service-' + blueDevice.blue_device_serial);
+
+  class PhCharacteristic extends api.hap.Characteristic {
     constructor() {
-      super('pH', uuid);
+      super('pH', phCharacteristicUUID);
       this.setProps({
-        format: Characteristic.Formats.FLOAT,
+        format: api.hap.Characteristic.Formats.FLOAT,
         unit: '',
         minValue: 0,
         maxValue: 20,
-        minStep: 1,
-        perms: [api.hap.Characteristic.Perms.PAIRED_READ, api.hap.Characteristic.Perms.NOTIFY],
+        minStep: 0.01,
+        perms: [
+          api.hap.Characteristic.Perms.PAIRED_READ,
+          api.hap.Characteristic.Perms.NOTIFY,
+        ],
       });
       this.value = this.getDefaultValue();
     }
-  };
-};
-
-// Define a custom PH Sensor Service
-export class PhSensorService extends this.api.hap.Service {
-  constructor(displayName: string, subtype?: string) {
-    super(displayName, PhServiceUUID, subtype);
-    this.addCharacteristic(PhCharacteristic);
   }
-}
 
-// ORP
-export function createOrpCharacteristic(api, blueDevice) {
-  const uuid = api.hap.uuid.generate('ORP-' + blueDevice.blue_device_serial);
-  return class OrpCharacteristic extends api.hap.Characteristic {
+  class PhSensorService extends api.hap.Service {
+    constructor(displayName: string, subtype?: string) {
+      super(displayName, phServiceUUID, subtype);
+      this.addCharacteristic(PhCharacteristic);
+    }
+  }
+
+  // ORP
+  const orpCharacteristicUUID = api.hap.uuid.generate('orp-' + blueDevice.blue_device_serial);
+  const orpServiceUUID = api.hap.uuid.generate('orp-service-' + blueDevice.blue_device_serial);
+
+  class OrpCharacteristic extends api.hap.Characteristic {
     constructor() {
-      super('Orp', uuid);
+      super('ORP', orpCharacteristicUUID);
       this.setProps({
-        format: Characteristic.Formats.FLOAT,
+        format: api.hap.Characteristic.Formats.FLOAT,
         unit: 'mV',
         minValue: 0,
         maxValue: 2000,
         minStep: 1,
-        perms: [api.hap.Characteristic.Perms.PAIRED_READ, api.hap.Characteristic.Perms.NOTIFY],
+        perms: [
+          api.hap.Characteristic.Perms.PAIRED_READ,
+          api.hap.Characteristic.Perms.NOTIFY,
+        ],
       });
       this.value = this.getDefaultValue();
     }
-  };
-};
-
-// Define a custom ORP Sensor Service
-export class OrpSensorService extends this.api.hap.Service {
-  constructor(displayName: string, subtype?: string) {
-    super(displayName, OrpServiceUUID, subtype);
-    this.addCharacteristic(OrpCharacteristic);
   }
+
+  class OrpSensorService extends api.hap.Service {
+    constructor(displayName: string, subtype?: string) {
+      super(displayName, orpServiceUUID, subtype);
+      this.addCharacteristic(OrpCharacteristic);
+    }
+  }
+
+  return {
+    ConductivityCharacteristic,
+    ConductivitySensorService,
+    PhCharacteristic,
+    PhSensorService,
+    OrpCharacteristic,
+    OrpSensorService,
+  };
 }
-  
-// --- Custom Services ---
-ConductivitySensorService.prototype = Object.create(Service.prototype);
-ConductivitySensorService.prototype.constructor = ConductivitySensorService;
-
-PhSensorService.prototype = Object.create(Service.prototype);
-PhSensorService.prototype.constructor = PhSensorService;
-
-OrpSensorService.prototype = Object.create(Service.prototype);
-OrpSensorService.prototype.constructor = OrpSensorService;
